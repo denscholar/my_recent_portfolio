51d355cf80e5438a4b034eef8ced45a0a9398a8c
Dennis Akagha <d.akagha20@gmail.com>
2024-10-30 00:32:07 +0100

    add hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Logo from '../../assets/profile_image.jpg'
+import '../Hero/Hero.css';
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <div className='hero'>
+            <div className="image-container">
+                <img src={Logo} alt="" />
+            </div>
+            <h1><span>I'm Dennis</span>, Full-stack Software Engineer</h1>
+            <p>With over 4 years of experience</p>
+            <duv className="hero-action">
+                <div className="hero-connect">Connect with me</div>
+                <div className="hero-resume">My Resume</div>
+            </duv>
+        </div>
+    )
 }
 
 export default Hero
