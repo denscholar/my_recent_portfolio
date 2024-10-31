6d32a43d5fb888a20bc632f254bfa1ada3a68a59
Dennis Akagha <d.akagha20@gmail.com>
2024-10-31 08:47:00 +0100

    make corrections.

--- src/components/Services/service_data.js
+++ src/components/Services/service_data.js
@@ -2,32 +2,32 @@ const serviceData = [
   {
     s_no: "01",
     title: "Web App Development",
-    description: "Building responsive and scalable web applications for seamless user experiences.",
+    description: "I build responsive and scalable web applications for seamless user experiences.",
   },
   {
     s_no: "02",
     title: "UI/UX",
-    description: "Crafting intuitive and visually appealing interfaces that enhance usability.",
+    description: "I craft intuitive and visually appealing interfaces that enhance usability.",
   },
   {
     s_no: "03",
     title: "Mobile App Development",
-    description: "Creating high-performance mobile applications for iOS and Android platforms.",
+    description: "I create high-performance mobile applications for iOS and Android platforms.",
   },
   {
     s_no: "04",
     title: "Data Analytics",
-    description: "Transforming data into actionable insights to drive smarter decisions.",
+    description: "I transform data into actionable insights to drive smarter decisions.",
   },
   {
     s_no: "05",
     title: "Online Training",
-    description: "Providing expert-led virtual courses to boost skills and career growth.",
+    description: "I provide expert-led virtual courses to boost skills and career growth.",
   },
   {
     s_no: "06",
     title: "Content Creation",
-    description: "Developing engaging and impactful content for digital platforms.",
+    description: "I develop engaging and impactful content for digital platforms.",
   },
 ];
 
